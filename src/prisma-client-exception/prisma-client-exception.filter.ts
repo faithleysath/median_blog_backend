1e0410a127f2296c41752446b4de318180f31e93
laysath <faithleysath@gmail.com>
2023-08-06 16:55:42 +0800

    :sparkles: feat: throw not found exception

---
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -0,0 +1,13 @@
+import { ArgumentsHost, Catch } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    console.error(exception.message);
+
+    // default to 500 server error
+    super.catch(exception, host);
+  }
+}
