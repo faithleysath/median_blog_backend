6bb114f4bc83cd5efa6b70026f98578c8740585d
laysath <faithleysath@gmail.com>
2023-08-06 16:00:28 +0800

    :rocket: chore: swagger,dto,entity

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -10,32 +10,51 @@ import {
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiTags, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { ArticleEntity } from './entities/article.entity';
 
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
+  @ApiTags('articles')
+  @ApiCreatedResponse({ type: ArticleEntity })
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
   }
 
   @Get()
+  @ApiTags('articles')
+  @ApiOkResponse({ type: [ArticleEntity] })
   findAll() {
     return this.articlesService.findAll();
   }
 
+  @Get('drafts')
+  @ApiTags('articles')
+  @ApiOkResponse({ type: [ArticleEntity] })
+  findDrafts() {
+    return this.articlesService.findDrafts();
+  }
+
   @Get(':id')
+  @ApiTags('articles')
+  @ApiOkResponse({ type: ArticleEntity })
   findOne(@Param('id') id: string) {
     return this.articlesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiTags('articles')
+  @ApiOkResponse({ type: ArticleEntity })
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articlesService.update(+id, updateArticleDto);
   }
 
   @Delete(':id')
+  @ApiTags('articles')
+  @ApiOkResponse({ type: ArticleEntity })
   remove(@Param('id') id: string) {
     return this.articlesService.remove(+id);
   }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -11,18 +11,25 @@ export class ArticlesService {
   }
 
   findAll() {
-    return `This action returns all articles`;
+    return this.prisma.article.findMany({ where: { published: true } });
+  }
+
+  findDrafts() {
+    return this.prisma.article.findMany({ where: { published: false } });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} article`;
+    return this.prisma.article.findUnique({ where: { id } });
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return this.prisma.article.delete({ where: { id } });
   }
 }
